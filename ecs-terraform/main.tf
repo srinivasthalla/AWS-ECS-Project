f2f20e6737948aec3b23bf393ed23ef9ef237ba6
Srinivas Thalla <157224356+srinivasthalla@users.noreply.github.com>
2025-11-04 15:15:05 +0530

    Update main.tf

--- ecs-terraform/main.tf
+++ ecs-terraform/main.tf
@@ -4,7 +4,8 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket  = "ecs-terraform-bucker000125457222363 " # create s3 bucket to store statefile
+    bucket  = "
+amn-s3-demo-bucket-31051995" # create s3 bucket to store statefile
     key     = "dev/terraform.tfstate"
     region  = "us-east-1"
     encrypt = false
@@ -48,4 +49,5 @@ module "ecs" {
   alb_listener_arn    = module.alb.listener_arn
 
 }
+
 
